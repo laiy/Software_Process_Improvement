551501f806892d73dc84acef4431d96b5945fa16
laiy <ly.franky@gmail.com>
2015-03-20 21:39:50 +0800

    Derp

--- lab3/sortableAndFilterable/merge.js
+++ lab3/sortableAndFilterable/merge.js
@@ -4,16 +4,66 @@
         return document.getElementsByTagName('table');
     };
 
-    var makeAllTablesSortable = function(tables) {
+    var makeAllTablesSortableAndFilterable = function(tables) {
         for (var tableIndex in tables) {
             if (!isNaN(tableIndex)) {
-                var ths = tables[tableIndex].getElementsByTagName('th');
-                addEventHandler(tables[tableIndex], ths);
+                makeFilterable(makeSortable(tables[tableIndex], tableIndex), tableIndex);
             }
         }
     };
 
-    var addEventHandler = function(table, ths) {
+    var makeFilterable = function(table, tableIndex) {
+        var input = document.createElement('input');
+        tables[tableIndex].parentNode.insertBefore(input, tables[tableIndex]);
+        filterAddEventHandler(tables[tableIndex], input);
+        return table;
+    };
+
+    var filterAddEventHandler = function(table, input) {
+        input.oninput = function() {
+            var inputValue = input.value;
+            matchAndHideAlongWithHighlightTheMatchedText(table, inputValue);
+        };
+    };
+
+    var matchAndHideAlongWithHighlightTheMatchedText = function(table, inputValue) {
+        var trs = table.getElementsByTagName('tr');
+        for (var trIndex in trs) {
+            if (!isNaN(trIndex) && trIndex !== "0") {
+                var tds = trs[trIndex].getElementsByTagName('td');
+                var matched = false;
+                for (var tdIndex in tds) {
+                    if (!isNaN(tdIndex)) {
+                        clearHightlight(tds[tdIndex]);
+                        if (tds[tdIndex].innerHTML.indexOf(inputValue) >= 0) {
+                            matched = true;
+                            highlightText(tds[tdIndex], inputValue);
+                        }
+                    }
+                }
+                trs[trIndex].style.display = matched ? 'table-row' : 'none';
+            }
+        }
+    };
+
+    var clearHightlight = function(td) {
+        var clearString = td.innerHTML.replace(/<[^>]+>/g, "");
+        td.innerHTML = clearString;
+    };
+
+    var highlightText = function(td, inputValue) {
+        var innerHTML = td.innerHTML;
+        var index = innerHTML.indexOf(inputValue);
+        td.innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index + inputValue.length) + "</span>" + innerHTML.substring(index + inputValue.length, innerHTML.length);
+    };
+
+    var makeSortable = function(table, tableIndex) {
+        var ths = tables[tableIndex].getElementsByTagName('th');
+        sortableAddEventHandler(tables[tableIndex], ths);
+        return table;
+    };
+
+    var sortableAddEventHandler = function(table, ths) {
         for (var thIndex in ths) {
             if (!isNaN(thIndex)) {
                 ths[thIndex].onclick = (function(thIndex) {
@@ -111,6 +161,6 @@
     };
 
     var tables = getAllTables();
-    makeAllTablesSortable(tables);
+    makeAllTablesSortableAndFilterable(tables);
 
 })();

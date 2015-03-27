4ea55b3bde9cc0fa82ba44cbaa790b991bc09607
laiy <ly.franky@gmail.com>
2015-03-27 19:16:30 +0800

    Done

--- lab4/S5/index.js
+++ lab4/S5/index.js
@@ -14,53 +14,63 @@ window.onload = function() {
 };
 
 var aHandler = function(currentSum, order) {
-    document.getElementById('message').innerHTML = "A:这是一个天大的秘密";
+    if (document.getElementsByClassName('apb')[0].getAttribute('clicked') === 'true') {
+        document.getElementById('message').innerHTML = "A:这是一个天大的秘密";
+    }
     var button = document.getElementById('control-ring').getElementsByTagName('li')[0];
     if (button.getAttribute('disabled') === "true") return;
     button.getElementsByTagName('span')[1].style.opacity = '1';
     button.getElementsByTagName('span')[1].innerHTML = '...';
     var disabledId = disableButtonExcept(button);
-    askForRandomNumberFromServer(button, disabledId, currentSum, order);
+    askForRandomNumberFromServer(button, disabledId, currentSum, order, 'A');
 };
 
 var bHandler = function(currentSum, order) {
-    document.getElementById('message').innerHTML = "B:我不知道";
+    if (document.getElementsByClassName('apb')[0].getAttribute('clicked') === 'true') {
+        document.getElementById('message').innerHTML = "B:我不知道";
+    }
     var button = document.getElementById('control-ring').getElementsByTagName('li')[1];
     if (button.getAttribute('disabled') === "true") return;
     button.getElementsByTagName('span')[1].style.opacity = '1';
     button.getElementsByTagName('span')[1].innerHTML = '...';
     var disabledId = disableButtonExcept(button);
-    askForRandomNumberFromServer(button, disabledId, currentSum, order);
+    askForRandomNumberFromServer(button, disabledId, currentSum, order, 'B');
 };
 
 var cHandler = function(currentSum, order) {
-    document.getElementById('message').innerHTML = "C:你不知道";
+    if (document.getElementsByClassName('apb')[0].getAttribute('clicked') === 'true') {
+        document.getElementById('message').innerHTML = "C:你不知道";
+    }
     var button = document.getElementById('control-ring').getElementsByTagName('li')[2];
     if (button.getAttribute('disabled') === "true") return;
     button.getElementsByTagName('span')[1].style.opacity = '1';
     button.getElementsByTagName('span')[1].innerHTML = '...';
     var disabledId = disableButtonExcept(button);
-    askForRandomNumberFromServer(button, disabledId, currentSum, order);
+    askForRandomNumberFromServer(button, disabledId, currentSum, order, 'C');
 };
 
 var dHandler = function(currentSum, order) {
-    document.getElementById('message').innerHTML = "D:他不知道";
+    if (document.getElementsByClassName('apb')[0].getAttribute('clicked') === 'true') {
+        document.getElementById('message').innerHTML = "D:他不知道";
+    }
     var button = document.getElementById('control-ring').getElementsByTagName('li')[3];
     if (button.getAttribute('disabled') === "true") return;
     button.getElementsByTagName('span')[1].style.opacity = '1';
     button.getElementsByTagName('span')[1].innerHTML = '...';
     var disabledId = disableButtonExcept(button);
-    askForRandomNumberFromServer(button, disabledId, currentSum, order);
+    askForRandomNumberFromServer(button, disabledId, currentSum, order, 'D');
 };
 
 var eHandler = function(currentSum, order) {
-    document.getElementById('message').innerHTML = "E:才怪";
+    if (document.getElementsByClassName('apb')[0].getAttribute('clicked') === 'true') {
+        document.getElementById('message').innerHTML = "E:才怪";
+    }
     var button = document.getElementById('control-ring').getElementsByTagName('li')[4];
     if (button.getAttribute('disabled') === "true") return;
     button.getElementsByTagName('span')[1].style.opacity = '1';
     button.getElementsByTagName('span')[1].innerHTML = '...';
     var disabledId = disableButtonExcept(button);
-    askForRandomNumberFromServer(button, disabledId, currentSum, order);
+    askForRandomNumberFromServer(button, disabledId, currentSum, order, 'E');
 };
 
 var disableButtonExcept = function(button) {
@@ -76,12 +86,12 @@ var disableButtonExcept = function(button) {
     return disabledId;
 };
 
-var askForRandomNumberFromServer = function(button, disabledId, currentSum, order) {
+var askForRandomNumberFromServer = function(button, disabledId, currentSum, order, caller) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             button.getElementsByTagName('span')[1].innerHTML = xmlhttp.responseText;
-            button.setAttribute('disabled', true);
+            button.setAttribute('disabled', 'true');
             button.style.backgroundColor = '#686868';
             var buttons = document.getElementById('control-ring').getElementsByTagName('li');
             for (var key in disabledId) {
@@ -90,44 +100,68 @@ var askForRandomNumberFromServer = function(button, disabledId, currentSum, orde
                     buttons[disabledId[key]].style.backgroundColor = 'rgba(48, 63, 159, 1)';
                 }
             }
-            checkIfAllButtonGotAResultAndAbleTheBigOneIfSo();
             var atplusButton = document.getElementsByClassName('apb')[0];
             if (atplusButton.getAttribute('clicked') === "true") {
-                currentSum += parseInt(xmlhttp.responseText);
-                switch (order[0]) {
-                    case 'A':
-                        order.shift();
-                        aHandler(0, order);
-                        break;
-                    case 'B':
-                        order.shift();
-                        bHandler(0, order);
-                        break;
-                    case 'C':
-                        order.shift();
-                        cHandler(0, order);
-                        break;
-                    case 'D':
-                        order.shift();
-                        dHandler(0, order);
-                        break;
-                    case 'E':
-                        order.shift();
-                        eHandler(0, order);
-                        break;
-                    default:
-                        var bigButton = document.getElementById('info-bar');
-                        bigButton.click();
-                        var result = 0;
-                        var buttons = document.getElementById('control-ring').getElementsByTagName('li');
-                        for (var key in buttons) {
-                            if (!isNaN(key)) {
-                                result += parseInt(buttons[key].getElementsByTagName('span')[1].innerHTML);
+                var err = Math.round(Math.random());
+                if (err) {
+                    button.setAttribute('disabled', 'false');
+                    button.style.backgroundColor = "rgba(48, 63, 159, 1)";
+                    checkIfAllButtonGotAResultAndAbleTheBigOneIfSo();
+                    switch (caller) {
+                        case 'A':
+                            aHandler(currentSum, order);
+                            break;
+                        case 'B':
+                            bHandler(currentSum, order);
+                            break;
+                        case 'C':
+                            cHandler(currentSum, order);
+                            break;
+                        case 'D':
+                            dHandler(currentSum, order);
+                            break;
+                        case 'E':
+                            eHandler(currentSum, order);
+                            break;
+                    }
+                } else {
+                    checkIfAllButtonGotAResultAndAbleTheBigOneIfSo();
+                    currentSum += parseInt(xmlhttp.responseText);
+                    switch (order[0]) {
+                        case 'A':
+                            order.shift();
+                            aHandler(currentSum, order);
+                            break;
+                        case 'B':
+                            order.shift();
+                            bHandler(currentSum, order);
+                            break;
+                        case 'C':
+                            order.shift();
+                            cHandler(currentSum, order);
+                            break;
+                        case 'D':
+                            order.shift();
+                            dHandler(currentSum, order);
+                            break;
+                        case 'E':
+                            order.shift();
+                            eHandler(currentSum, order);
+                            break;
+                        default:
+                            var bigButton = document.getElementById('info-bar');
+                            bigButton.click();
+                            var result = 0;
+                            for (key in buttons) {
+                                if (!isNaN(key)) {
+                                    result += parseInt(buttons[key].getElementsByTagName('span')[1].innerHTML);
+                                }
                             }
-                        }
-                        atplusButton.setAttribute('clicked', "true");
-                        document.getElementById('message').innerHTML = "大气泡:楼主异步调用战斗力感人，目测不超过" + result;
-                        break;
+                            atplusButton.setAttribute('clicked', "true");
+                            document.getElementById('message').innerHTML = "大气泡:楼主异步调用战斗力感人，目测不超过" + result;
+                            break;
+                    }
+
                 }
             }
         }
